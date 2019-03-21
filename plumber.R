21e9d8049a2edcee4025a400b0a6b7c789ba90e7
Chris Beeley <chris.beeley@gmail.com>
2019-03-21 15:22:54 +0000

    Early work getting the team reports over a plumber API

--- plumber.R
+++ plumber.R
@@ -1,4 +1,21 @@
 
+#* @serializer contentType list(type="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+#* @get /team
+function(team){
+  tmp <- tempfile()
+  
+  area_name_team <- counts %>% 
+    filter(TeamC %in% team) %>% 
+    pull(TeamN) %>% 
+    unique() %>% 
+    paste(collapse = ", ")
+  
+  render("reports/team_quarterly.Rmd", tmp, output_format = "word_document",
+         params = list(team = team, area_name = area_name_team))
+
+  readBin(tmp, "raw", n=file.info(tmp)$size)
+}
+
 # okay, what's the plan here?
 # let's set up two /URLS
 # one for a team quarterly report at /team
@@ -6,33 +23,23 @@
 # one at /custom which accepts other parameters
 
 # this returns a word document with a team report in
-#* @serializer contentType list(type="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-#* @get /team
-function(team){
-  tmp <- tempfile()
-  
-  render("test_report.Rmd", tmp, output_format = "word_document",
-         params = list(team = team))
 
-  readBin(tmp, "raw", n=file.info(tmp)$size)
-}
-
-# this returns an html report with a team report in
-#* @serializer contentType list(type="text/html; charset=utf-8")
-#* @get /team-html
-function(team){
-  tmp <- tempfile()
-  
-  render("test_report.Rmd", tmp, output_format = "html_document",
-         params = list(team = team))
-  
-  readBin(tmp, "raw", n=file.info(tmp)$size)
-}
-
-# this returns a custom report just for kicks
-#* @serializer contentType list(type="application/html")
-#* @get /custom
-function(res){
-  
-  include_rmd("test.Rmd", res)
-}
+# # this returns an html report with a team report in
+# #* @serializer contentType list(type="text/html; charset=utf-8")
+# #* @get /team-html
+# function(team){
+#   tmp <- tempfile()
+#   
+#   render("test_report.Rmd", tmp, output_format = "html_document",
+#          params = list(team = team))
+#   
+#   readBin(tmp, "raw", n=file.info(tmp)$size)
+# }
+# 
+# # this returns a custom report just for kicks
+# #* @serializer contentType list(type="application/html")
+# #* @get /custom
+# function(res){
+#   
+#   include_rmd("test.Rmd", res)
+# }
