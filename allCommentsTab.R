3c583efcb3acda6cb1de4c7cbde1bca40b5a3942
Chris Beeley <chris.beeley@gmail.com>
2019-05-05 19:53:14 +0100

    Factorised the code that writes the markdown reports into report_functions.R and added a download comments button to the all comments tab.
    Factorising was a lot of work but I think it will be worth it because I haven't finished with the outputs on the all comments tab and there would have been even more duplicated code in a few months

--- allCommentsTab.R
+++ allCommentsTab.R
@@ -1,135 +1,89 @@
 
 output$allImproveComments = renderText({
   
-  # this first bit is for if they are ordering by category
+  df <- allComments(passData()[["currentData"]], input$sortCategoryCriticality, "Improve")
   
   if(input$sortCategoryCriticality == "Category"){
     
-    # going to left join the improve codes to the category table to produce
-    # a fully labelled dataframe with all comments, sub and super categories
-    
-    df = passData()[["currentData"]] %>% 
-      filter(!is.na(Imp1), !is.na(Improve)) %>% 
-      left_join(categoriesTable, by = c("Imp1" = "Number")) %>%  
-      select(key : Time, Improve: CommentCoderBest, 
-             Location, Division, Directorate, Division2 : type) %>% 
-      filter(!is.na(Super)) %>% 
-      group_by(Super) %>% 
-      mutate(count = n()) %>% 
-      arrange(-count, Location)
-    
     finalText = map(unique(df$Super), function(x) {
       
-      commentsFrame = df %>% filter(Super == x) %>% select(Improve, Location)
+      commentsFrame = df %>% 
+        filter(Super == x) %>% 
+        ungroup() %>% 
+        select(Improve, Location) %>% 
+        set_names(c("Comment", "Location"))
       
       paste0("<h3>", x, "</h3>", 
-             paste0("<p>", commentsFrame$Improve, " (", 
+             paste0("<p>", commentsFrame$Comment, " (", 
                     commentsFrame$Location, ")</p>", collapse = "")
       )
     })
     
     return(unlist(finalText))
-  }
-  
-  # now let's do a whole other thing for if they want to sort by criticality
-  
-  if(input$sortCategoryCriticality == "Criticality"){
     
-    # going to left join the improve codes to the category table to produce
-    # a fully labelled dataframe with all comments, sub and super categories
-    
-    df = passData()[["currentData"]] %>% 
-      filter(!is.na(ImpCrit), !is.na(Improve)) %>% 
-      select(key : Time, Improve: CommentCoderBest, 
-             Location, Division, Directorate, Division2) %>% 
-      arrange(Location)
-    
-    # make a vector to store the names in
-    
+  } else {
     nameVector = c("Mildly critical", "Somewhat critical", "Very critical")
     
     finalText = map(3 : 1, function(x) {
       
-      commentsFrame = df %>% filter(ImpCrit == x) %>% select(Improve, Location)
+      commentsFrame = df %>% 
+        filter(ImpCrit == x) %>% 
+        select(Improve, Location) %>% 
+        set_names(c("Comment", "Location"))
       
       if(nrow(commentsFrame) > 0){
         
-        paste0("<h3>", nameVector[x], "</h3>", 
-               paste0("<p>", commentsFrame$Improve, " (", 
+        paste0("<h3>", nameVector[x], "</h3>",
+               paste0("<p>", commentsFrame$Comment, " (",
                       commentsFrame$Location, ")</p>", collapse = "")
         )
       }
     })
-    
     return(unlist(finalText))
   }
 })
 
-# this code is, very shamefully, just copied from above and changed to 
-# best thing. It's just easier than making it into a function
-
 output$allBestComments = renderText({
   
-  # this first bit is for if they are ordering by category
+  df <- allComments(passData()[["currentData"]], input$sortCategoryCriticality, "Best")
   
   if(input$sortCategoryCriticality == "Category"){
     
-    # going to left join the best codes to the category table to produce
-    # a fully labelled dataframe with all comments, sub and super categories
-    
-    df = passData()[["currentData"]] %>% 
-      filter(!is.na(Best1), !is.na(Best)) %>% 
-      left_join(categoriesTable, by = c("Best1" = "Number")) %>%  
-      select(key : Time, Improve: CommentCoderBest, 
-             Location, Division, Directorate, Division2 : type) %>% 
-      filter(!is.na(Super)) %>% 
-      group_by(Super) %>% 
-      mutate(count = n()) %>% 
-      arrange(-count, Location)
-    
-    finalText = map(unique(df$Super), function(x) {
+    finalText <- map(unique(df$Super), function(x) {
       
-      commentsFrame = df %>% filter(Super == x) %>% select(Best, Location)
+      commentsFrame <- df %>% 
+        filter(Super == x) %>% 
+        ungroup() %>% 
+        select(Best, Location) %>% 
+        set_names(c("Comment", "Location"))
       
       paste0("<h3>", x, "</h3>", 
-             paste0("<p>", commentsFrame$Best, " (", 
+             paste0("<p>", commentsFrame$Comment, " (", 
                     commentsFrame$Location, ")</p>", collapse = "")
       )
     })
     
     return(unlist(finalText))
-  }
-  
-  # now let's do a whole other thing for if they want to sort by criticality
-  
-  if(input$sortCategoryCriticality == "Criticality"){
     
-    # going to left join the best codes to the category table to produce
-    # a fully labelled dataframe with all comments, sub and super categories
-    
-    df = passData()[["currentData"]] %>% 
-      filter(!is.na(BestCrit), !is.na(Best)) %>% 
-      select(key : Time, Improve: CommentCoderBest, 
-             Location, Division, Directorate, Division2) %>% 
-      arrange(Location)
-    
-    # make a vector to store the names in
+  } else {
     
     nameVector = c("Mildly complimentary", "Somewhat complimentary", "Very complimentary")
     
     finalText = map(3 : 1, function(x) {
       
-      commentsFrame = df %>% filter(BestCrit == x) %>% select(Best, Location)
+      commentsFrame = df %>% 
+        filter(BestCrit == x) %>% 
+        select(Best, Location) %>% 
+        set_names(c("Comment", "Location"))
       
       if(nrow(commentsFrame) > 0){
         
-        paste0("<h3>", nameVector[x], "</h3>", 
-               paste0("<p>", commentsFrame$Best, " (", 
+        paste0("<h3>", nameVector[x], "</h3>",
+               paste0("<p>", commentsFrame$Comment, " (",
                       commentsFrame$Location, ")</p>", collapse = "")
         )
       }
     })
-    
     return(unlist(finalText))
   }
 })
@@ -140,8 +94,98 @@ output$downloadAllComments <- downloadHandler(
   filename = "AllComments.docx",
   content = function(file){
     
+    if(nrow(passData()[["currentData"]]) > 10000){
+      
+      showModal(
+        modalDialog(
+          title = "Error!",
+          HTML("Too many comments, please reduce your selection"),
+          easyClose = TRUE
+        )
+      )
+      
+      return()
+    }
+    
+    # Set up parameters to pass to Rmd document
+    
+    # find which areas are selected
+    
+    report_area <- case_when(
+      isTruthy(input$selTeam) ~ "team",
+      isTruthy(input$selDirect) ~ "directorate",
+      isTruthy(input$Division) ~ "division",
+      TRUE ~ "trust"
+    )
+    
+    if(report_area == "trust"){
+      
+      area_name <- "the whole Trust"
+      
+      params <- list(division = "NA",
+                     carerSU = input$carerSU,
+                     area_name = area_name,
+                     date_from = input$dateRange[1],
+                     date_to = input$dateRange[2],
+                     comment_sort = input$sortCategoryCriticality)
+    }
+    
+    if(report_area == "division"){
+      
+      area_name <- c("Local Partnerships- Mental Healthcare", 
+                     "Forensic Services", 
+                     "Local Partnerships- Community Healthcare")[as.numeric(input$Division) + 1]
+      
+      params <- list(division = input$Division,
+                     carerSU = input$carerSU,
+                     area_name = area_name,
+                     date_from = input$dateRange[1],
+                     date_to = input$dateRange[2],
+                     comment_sort = input$sortCategoryCriticality)
+    }
+    
+    if(report_area == "directorate"){
+      
+      first_date <- input$dateRange[1]
+      
+      end_date <- input$dateRange[2]
+      
+      number_rows = trustData %>%
+        filter(Directorate %in% input$selDirect) %>% 
+        filter(Date >= first_date, Date <= end_date) %>% 
+        nrow()
+      
+      area_name <- dirTable %>% 
+        filter(DirC %in% input$selDirect) %>% 
+        pull(DirT) %>% 
+        paste(collapse = ", ")
+      
+      params <- list(directorate = input$selDirect,
+                     carerSU = input$carerSU,
+                     area_name = area_name,
+                     date_from = input$dateRange[1],
+                     date_to = input$dateRange[2],
+                     comment_sort = input$sortCategoryCriticality)
+    }
+    
+    if(report_area == "team"){
+      
+      area_name_team <- passData()[["currentData"]] %>% 
+        pull(TeamN) %>% 
+        unique() %>% 
+        paste(collapse = ", ")
+      
+      params <- list(team = input$selTeam,
+                     carerSU = input$carerSU,
+                     area_name = area_name_team,
+                     date_from = input$dateRange[1],
+                     date_to = input$dateRange[2],
+                     comment_sort = input$sortCategoryCriticality)
+    }
+    
     render("reports/AllComments.Rmd", output_format = "word_document",
-           quiet = TRUE, envir = environment())
+           quiet = TRUE, params = params,
+           envir = new.env(parent = globalenv()))
     
     # copy docx to 'file'
     file.copy("reports/AllComments.docx", file, overwrite = TRUE)
