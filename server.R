2083ae27ac61884b4e2e30ede6e507d7aea5c15b
Spreadborough Stephen - Senior Data Scientist <S.Spreadborough@nottshc.nhs.uk>
2024-03-04 10:52:11 +0000

    removed notification

--- server.R
+++ server.R
@@ -26,18 +26,18 @@ function(input, output, session){
   source("demographics.R", local = TRUE)
   # source("topicExplorer.R", local = TRUE)
   
-  showNotification(
-    "Please note: some responses stating 'nothing to improve' or 
-    'nothing was good' have been re-coded to give consistent criticality values.
-    These comments have also been removed from counts of comments by 
-    'What could be improved?' and 'Best thing' to support interpretation of this
-    information. These updates have mainly 
-    changed the proportions of sub-categories in the 'Comments' page under 
-    'What could be Improved?', where 'Miscellaneous: Nothing to Improve' was 
-    previously the main sub-category but has now been removed. ", 
-    duration = 60, 
-    type = "warning"
-  )
+  #showNotification(
+  #  "Please note: some responses stating 'nothing to improve' or 
+  #  'nothing was good' have been re-coded to give consistent criticality values.
+  #  These comments have also been removed from counts of comments by 
+  #  'What could be improved?' and 'Best thing' to support interpretation of this
+  #  information. These updates have mainly 
+  #  changed the proportions of sub-categories in the 'Comments' page under 
+  #  'What could be Improved?', where 'Miscellaneous: Nothing to Improve' was 
+  #  previously the main sub-category but has now been removed. ", 
+  #  duration = 60, 
+  #  type = "warning"
+  #)
   
   # are they logged in?
   
